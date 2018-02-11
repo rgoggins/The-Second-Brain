e7c2ab23f8b237489d9126a7a38a56c00b566244
Ryan Goggins <ryangoggins@Ryans-MBP.andover.edu>
2018-02-11 14:46:17 -0500

    Handling user input

--- The Second Brain/ViewController.swift
+++ The Second Brain/ViewController.swift
@@ -13,8 +13,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Things added
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tapHandler = UITapGestureRecognizer(target: self, action: Selector("respond"))
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: Selector("respondToSwipeGesture:"))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: Selector("respondToSwipeGesture:"))
+        swipeDown.direction = UISwipeGestureRecognizerDirection.down
+        self.view.addGestureRecognizer(swipeDown)
+        
+        print("going")
+    }
+    
+    func respondToSwipeGesture(_ gesture: UIGestureRecognizer) -> Void {
+        
+        print("actually entered into respond to swipe gesture stack")
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.right:
+                print("Swiped right")
+            case UISwipeGestureRecognizerDirection.down:
+                print("Swiped down")
+            case UISwipeGestureRecognizerDirection.left:
+                print("Swiped left")
+            case UISwipeGestureRecognizerDirection.up:
+                print("Swiped up")
+            default:
+                break
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
