00f5e98bf5d68a62b425c4f012521a4c446ea1bf
Ryan Goggins <ryangoggins@ryans-mbp.andover.edu>
2018-02-11 14:30:02 -0500

    Initial Commit

---
+++ The Second Brain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Second Brain
+//
+//  Created by Ryan Goggins on 2/11/18.
+//  Copyright © 2018 Ryan Goggins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ The Second BrainTests/The_Second_BrainTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Second_BrainTests.swift
+//  The Second BrainTests
+//
+//  Created by Ryan Goggins on 2/11/18.
+//  Copyright © 2018 Ryan Goggins. All rights reserved.
+//
+
+import XCTest
+@testable import The_Second_Brain
+
+class The_Second_BrainTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
