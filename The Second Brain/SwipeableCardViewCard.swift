c9e0e1b58fe36d08baf69eae435e077a17228803
Ahmet Oguzlu <aoguzlu18@andover.edu>
2018-02-12 20:28:52 -0500

    Improvements on the app
    - Added Left&Right Swipe Gesture Recognizers.
    - Added UI Constraints to make the main page compatible with different
    screen sizes and landscape mode.
    - Connected UI to code.
    - Replaced the TextField with a TextView let the user type multiple
    lines.
    - Commented out classes that caused fatal errors.

--- The Second Brain/SwipeableCardViewCard.swift
+++ The Second Brain/SwipeableCardViewCard.swift
@@ -1,18 +1,20 @@
 import UIKit
 
-class SwipeableCardViewCard: SwipeableView, NibView {
-    
-    //Adapted from github.com/phillfarrugia/swipeable-view-stack
-    
-    required init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//        xibSetup()
-    }
-    
-    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        xibSetup()
-    }
-    
-}
+// I'm not sure about the purpose of this class but it causes errors, so I commented everything out
+
+//class SwipeableCardViewCard: SwipeableView, NibView {
+//
+//    //Adapted from github.com/phillfarrugia/swipeable-view-stack
+//
+//    required init?(coder aDecoder: NSCoder) {
+////        super.init(coder: aDecoder)
+////        xibSetup()
+//    }
+//
+//    override init(frame: CGRect) {
+////        super.init(frame: frame)
+////        xibSetup()
+//    }
+//
+//}
 
